b1958321d541d89fbc1fafdac9f0f7acbe47d156
root <root@voip-a.gps.local>
2011-05-25 20:08:34 +1000

    Install script changes..

--- install.sh
+++ install.sh
@@ -1,16 +1,62 @@
 #!/bin/bash
 
-yum groupinstall "Development tools"
-yum install atrpms-repo    # For fxload, iksemel and spandsp
-yum install epel-release # for php-pear-DB, soon to be removed as a prereq.
-yum install libusb-devel 
-yum install fxload
-yum install iksemel iksemel-devel
-yum install httpd php php-fpdf
-yum install mysql-server
-yum install curl
-yum install mysql mysql-devel
-yum install php-pear-DB php-process
-yum install libxml2-devel ncurses-devel libtiff-devel libogg-devel
-yum install libvorbis vorbis-tools
+#yum -y groupinstall "Development tools"
+#yum -y install atrpms-repo    # For fxload, iksemel and spandsp
+#yum -y install epel-release # for php-pear-DB, soon to be removed as a prereq.
+#yum -y install libusb-devel 
+#yum -y install fxload
+#yum -y install iksemel iksemel-devel
+#yum -y install httpd php php-fpdf
+#yum -y install mysql-server
+#yum -y install curl
+#yum -y install mysql mysql-devel
+#yum -y install php-pear-DB php-process
+#yum -y install libxml2-devel ncurses-devel libtiff-devel libogg-devel
+#yum -y install libvorbis vorbis-tools
+
+# If /etc/hipbx.conf already exists, grab it and read the config
+SETUPOK=yes
+if [ -f /etc/hipbx.conf ]; then
+	. /etc/hipbx.conf
+	for checkvar in MYSQLPASS ISMASTER; do
+		if [ "${!checkvar}" = "" ]; then
+			echo \$$checkvar undefined. Restarting setup
+			SETUPOK=no
+		fi
+	done
+else
+	SETUPOK=no
+fi
+
+# Is this the master or slave server?
+if [ "$ISMASTER" = "" ]; then
+	echo -n "Is this the Master or Slave server? [M/s] "
+	read resp
+	if [ "$resp" = "" -o "$resp" = "M" -o "$resp" = "m" ]; then
+		ISMASTER=YES
+	else
+		ISMASTER=NO
+	fi
+fi
+echo ISMASTER=$ISMASTER >> /etc/hipbx.conf
+
+if [ $ISMASTER = NO ]; then
+	echo -n "Please enter IP Address of Master [10.80.17.1] "
+	read masterip
+	[ "$masterip" = "" ] && masterip="10.80.17.1"
+	if ping -c1 $masterip > /dev/null; then
+		echo "$slaveip able to ping"
+	else
+		echo  -e "Error: Unable to ping $masterip. Sorry.\nYou need to fix this. If this is a Master server, delete the /etc/hipbx.conf file and re-run install"
+		exit
+	fi
+fi
+	
+
+
+
+
+# Generate a MySQL password, if one hasn't already been generated.
+[ "$MYSQLPASS" = "" ] && MYSQLPASS=`tr -dc A-Za-z0-9 < /dev/urandom | head -c8`
+echo MYSQLPASS=$MYSQLPASS > /etc/hipbx.conf
 
