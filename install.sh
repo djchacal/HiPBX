5a344f4b5b93eeee4a4b791f227550507525e462
Rob Thomas <xrobau@gmail.com>
2011-06-17 11:39:32 +1000

    Fix missing AGPL licence on installer.

--- install.sh
+++ install.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-# tell them to disabled SELINUX. 
+#  HiPBX Installer Script. 
+#  Copyright 2011, Rob Thomas <xrobau@gmail.com>
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Check if SELinux is enabled. If so, disable it and warn that it's been
+# done.
 
 if selinuxenabled; then
 	echo "SELinux is enabled. I've turned it off for you. Be aware."
