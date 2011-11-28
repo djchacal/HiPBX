746a6c72d6abeeb3eef6fc9438d2b2681534237e
Rob Thomas <xrobau@gmail.com>
2011-11-28 19:26:24 +1000

    Tidyups.

--- addons/dahdi-management/ajax.php
+++ addons/dahdi-management/ajax.php
@@ -11,10 +11,10 @@ include("include.php");
 $sno = $_REQUEST['sno'];
 $xpd = $_REQUEST['xpd'];
 $port = $_REQUEST['port'];
+$ext = $_REQUEST['ext'];
+$cidname = $_REQUEST['cidname'];
+$tone = $_REQUEST['tone'];
 $action = $_REQUEST['action'];
-#$sno = 'X1056351';
-#$xpd = '00';
-#$port = '1';
 
 switch ($action) {
 	case "ext":
@@ -25,11 +25,16 @@ switch ($action) {
 		break;
 	case "ports":
 		show_ports($sno, $xpd);
-			
+		break;
+	case "addext":
+		addext($ext, $sno, $xpd, $port, $tone, $cidname);
+		break;
+	case "remove":
+		removeext($ext);
+		break;
 }
 
 function ajax_ext($sno, $xpd, $port) {
-
 	global $db;
 
 	# Dump everything we need to care about.
@@ -68,41 +73,16 @@ function ajax_ext($sno, $xpd, $port) {
 	
 	$ext = $db->getOne("select ext from provis_dahdi_ports where `serial`='$sno' and `xpd`='$xpd' and `portno`='$port'");
 	if ($ext == "") {
-		print "<span class='left'>CallerID Name</span>\n";
-		print "<span class='right'><input id='cidname' type=text size=15></span><br />\n";
-		print "<span class='left'>Extension</span>\n";
-		print "<span class='right'><input id='extno' type=text size=4></span><br />\n";
-		print "<span class='left'>Dial Tone</span>\n";
-		print "<span class='right'>\n";
-		print "  <input type='radio' name='tonezone' value='AU' checked >Au</input>\n";
-		print "  <input type='radio' name='tonezone' value='XX'>Loud</input>\n";
-		print "  <input type='radio' name='tonezone' value='YY'>Fax</input>\n";
-		print "</span><p id='addstat'>&nbsp;</p>\n";
-		print "<center><button id='addext' name='addext'>Add Ext</button></center>";
-		#print "<button id='delext name='delext'>Del Ext</button>'</center>\n";
-		#print "<center><button id='addext' name='addext'>Add Ext</button>&nbsp;&nbsp";
-		#print "<button id='delext name='delext'>Del Ext</button>'</center>\n";
-		exit;
+		showextpage(null, null, null);
+		print "<center><button id='addext' onClick='addext()'>Add Ext</button></center>";
 	} else {
 		$res=core_users_get($ext);
 		if (!isset($res['name'])) { ?>
 			<h3>FreePBX Error</h3>
 			<span>This extension <strong>does not exist</strong> in FreePBX. It's probably been deleted.
 			You may either remove this from provisioning, or re-create it.</span>
-			<br /><br /><center><button id='remove'>Remove</button>&nbsp;&nbsp;<button id='create'>Create</button></center>
-			<script>
-			$('#remove').bind('click', function() {
-				alert($('#astribank').data('sno'));
-				var query= {
-					sno:  $('#astribank').data('sno'),
-					xpd:  $('#astribank').attr('data-xpd'),
-					port: $('#astribank').data('port'),
-					action: 'remove',
-				};
-				$.post("doext.php", query, function(data) { $("#ctext").html(data); });
-
-			});
-			</script>
+			<br /><br /><center><button id='remove' onClick='removeext()'>Remove</button>&nbsp;&nbsp;
+			<button id='create' onClick='addext()'>Create</button></center>
 		<?php
 		exit;
 		}
@@ -148,4 +128,79 @@ function show_ports($sno, $xpd) {
 		print "<td id='port_$x'><div class='ext' id='port$x' data-sno='$sno' data-xpd='$xpd' data-portno='$x'>$str</div></td>\n";
 	}
 	print "</tr></table>\n";
+}
+
+function addext($ext, $sno, $xpd, $port, $tone, $cidname) {
+	global $db;
+
+	# Adding an Extension. Yay. 
+	# Rule 1: Sanity check ALL THE THINGS.
+	if ($ext === '') {
+		print "Extension can't be blank\n";
+		exit;
+	}
+	if ($cidname === '') {
+		$cidname = "Plant Phone\n";
+	}
+	# Now. Does this ext already exist?
+	$results = $db->getRow("SELECT extension,name FROM users where extension='$ext'", DB_FETCHROW_ASSOC);
+	if (isset($results[0])) {
+		print "Extension $ext already assigned to ".$results[1];
+		exit;
+	} 
+	# Nifty. Add it to our database.
+	$sql = "INSERT INTO `provis_dahdi_ports` (serial, xpd, portno, ext, tone) values ('$sno', '$xpd', '$port', '$ext', '$tone')";
+	$res = $db->query($sql);
+	if (PEAR::isError($res)) {
+		print "Unable to update database:<br />SQL: $sql<br /><pre>".$res->getMessage()."</pre>";
+		exit;
+	}
+	# OK. Now, lets figure out what ACTUAL dahdi port this thing is.
+	# What span number  is this?
+	$spanno = $db->getone("select span from provis_dahdi_spans where `serial`='$sno' and `xpd`='$xpd'");
+	# And how many ports are before this span? 
+	$baseno = $db->getOne("select SUM(ports) from provis_dahdi_spans where `span` < $spanno");
+	$dahdi = $baseno + $port;
+	print "Port $port This would be port DAHDI/$dahdi\n";
+	# Big array of vars that FreePBX cares about. Half of this stuff probably isn't needed.
+	$vars = array (
+		'display' => 'extensions', 'type' => 'setup', 'action' => 'add', 'extdisplay' => '', 'extension' => $ext, 'name' => $cidname,
+		'cid_masquerade' => '', 'sipname' => '', 'outboundcid' => '', 'ringtimer' => 0, 'cfringtimer' => 0, 'concurrency_limit' => 0,
+		'callwaiting' => 'enabled', 'answermode' => 'disabled', 'call_screen' => 0, 'pinless' => 'disabled', 'emergency_cid' => '', 
+		'tech' => 'dahdi', 'hardware' => 'generic', 'qnostate' => 'usestate', 'newdid_name' => '', 'newdid' => '', 'newdidcid' => '',
+		'devinfo_secret_origional' => '', 'devinfo_dtmfmode' => 'rfc2833', 'devinfo_canreinvite' => 'no',
+		'devinfo_context' => 'from-internal', 'devinfo_host' => 'dynamic', 'devinfo_trustrpid' => 'yes', 'devinfo_sendrpid' => 'no', 
+		'devinfo_type' => 'peer', 'devinfo_nat' => 'no', 'devinfo_port' => '5060', 'devinfo_qualify' => 'yes', 'devinfo_qualifyfreq' => '60',
+		'devinfo_transport' => 'udp', 'devinfo_encryption' => 'no', 'devinfo_callgroup' => '', 'devinfo_pickupgroup' => '',
+		'devinfo_disallow' => '', 'devinfo_allow' => '', 'devinfo_dial' => '', 'devinfo_accountcode' => '', 'devinfo_mailbox' => $ext.'@default',
+		'devinfo_vmexten' => '', 'devinfo_deny' => '0.0.0.0/0.0.0.0', 'devinfo_permit' => '0.0.0.0/0.0.0.0', 'noanswer_dest' => 'goto0', 
+		'busy_dest' => 'goto1', 'chanunavail_dest' => 'goto2', 'dictenabled' => 'disabled', 'dictformat' => 'ogg', 'dictemail' => '',
+		'langcode' => '', 'record_in' => 'Adhoc', 'record_out' => 'Adhoc', 'email' => $email, 'vm' => 'disabled', 
+	);
+	$vars['devinfo_dial']="DAHDI/$dahdi";
+	# And FreePBX Also wants them to be in $_REQUEST, too.
+	$_REQUEST=$vars;
+	core_users_add($vars);
+	core_devices_add($ext, 'dahdi', '', 'fixed', $ext, $name);
+} 
+
+function removeext($ext) {
+	print "<h2>Removing Extension</h2>\n";
+}
+
+# core_devices_add($deviceid,$tech,$devinfo_dial,$devicetype,$deviceuser,$description,$emergency_cid,true);
+
+
+
+function showextpage($ext, $name, $tone) {
+	print "<span class='left'>CallerID Name</span>\n";
+	print "<span class='right'><input id='cidname' type=text size=15></span><br />\n";
+	print "<span class='left'>Extension</span>\n";
+	print "<span class='right'><input id='extno' type=text size=4></span><br />\n";
+	print "<span class='left'>Dial Tone</span>\n";
+	print "<span class='right'>\n";
+	print "  <input type='radio' name='tonezone' value='AU' checked >Au</input>\n";
+	print "  <input type='radio' name='tonezone' value='XX'>Loud</input>\n";
+	print "  <input type='radio' name='tonezone' value='YY'>Fax</input>\n";
+	print "</span><p id='addstat'>&nbsp;</p>\n";
 }
--- addons/dahdi-management/dahdi.js
+++ addons/dahdi-management/dahdi.js
@@ -3,27 +3,27 @@ function bindall() {
 	$('.click').bind('click', function() {
 		var s=$(this).data('sno');
 		var x=$(this).attr('data-xpd'); // 00 = 0 if you use '.data'
-		showports(s, x);
+		showports(s, x, true);
 	});
 };
 
 
-function showports(s, x) {
+function showports(s, x, bounce) {
 	$.ajax({
 		type: 'POST',
 		url: 'ajax.php',
 		data: 'action=ports&sno='+s+'&xpd='+x,
 		beforeSend: function() {
-			$(".ports").hide("fast");
+			if (bounce) { $(".ports").hide("fast"); }
 			$("#olay").show();
 			$("#olay").spin("large", "white");
 		},
 		success: function( msg ) {  
 			$("#olay").spin(false);
 			$("#olay").hide();
-			$(".ports").hide('fast');
+			if (bounce) { $(".ports").hide('fast'); }
 			$("#"+s).html(msg);
-			$("#"+s).show('fast');
+			if (bounce) { $("#"+s).show('fast'); }
 			$(".ext").bind('click', function() { 
 				modport($(this).data('sno'), $(this).attr('data-xpd'), $(this).data('portno'));
 			});
@@ -45,37 +45,42 @@ function modport(ser, xpd, no) {
 			};
 			$.post("ajax.php", query, function(data) {
 				$('#'+$('#astribank').data('sno')+'_'+$('#astribank').attr('data-xpd')).html(data);
-				showports($('#astribank').data('sno'), $('#astribank').attr('data-xpd'));
+				showports($('#astribank').data('sno'), $('#astribank').attr('data-xpd'), false);
 				bindall();
 			});
-			$("#olay").hide(); },
+		},
 	});
 	$("#content").overlay().load();
 	$.get("ajax.php", 'action=ext&sno='+ser+'&xpd='+xpd+'&port='+no, function(data) {
 		$("#ctext").html(data);
-		$("#addext").bind('click', function() {
-			$('input:radio').each(function(){
-				if ($(this).attr('checked')) {
-					addext( $("#cidname").val(), $("#extno").val(), $(this).attr('value') );
-				}
-			});
-		});	
-	});
+	});	
 }
 
-function addext(cidname, ext, tone) {
+function addext() {
 	$("#addstat").html("<i>Processing...</i>");
+	$('input:radio').each(function(){
+		if ($(this).attr('checked')) {
+			var query= {
+				sno:  $('#astribank').data('sno'),
+				xpd:  $('#astribank').attr('data-xpd'),
+			  	port: $('#astribank').data('port'),
+				ext:  $("#extno").val(),
+				cidname: $("#cidname").val(),
+				tone:  $(this).attr('value'),
+				action: 'addext',
+			};
+		$.post("ajax.php", query, function(data) { $("#addstat").html(data); });
+		}
+	});
+} 
+
+function removeext() {
 	var query= {
 		sno:  $('#astribank').data('sno'),
 		xpd:  $('#astribank').attr('data-xpd'),
-	  	port: $('#astribank').data('port'),
-		ext:  ext,
-		cidname: cidname,
-		tone: tone,
-		action: 'addext',
+		port: $('#astribank').data('port'),
+		action: 'remove',
 	};
-	$.post("doext.php", query, function(data) {
-		$("#addstat").html(data);
-	});
+	$.post("ajax.php", query, function(data) { $("#ctext").html(data); });
 }
 
